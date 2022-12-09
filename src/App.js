0507cb55f4d54bfbe2b771b975781bc2698167cf
Matteo Digiorgio <matthew.digiorgio@gmail.com>
2022-12-09 13:47:23 +0100

    Basic sidebar implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
@@ -9,9 +10,11 @@ function App() {
       <Header />
 
       {/* APP BODY */}
-      {/* SIDEBAR */}
-      {/* FEED */}
-      {/* WIDGET */}
+      <div className="app__body">
+        <Sidebar />
+        {/* FEED */}
+        {/* WIDGET */}
+      </div>
     </div>
   );
 }
